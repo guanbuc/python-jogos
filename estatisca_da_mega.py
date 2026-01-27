6d2d3b8de486074d2a00c057c6afd45ce65c9bac
guanbuc <guanbuc@gmail.com>
2026-01-26 21:12:47 -0300

    subindo nova versão

--- estatisca_da_mega.py
+++ estatisca_da_mega.py
@@ -12,13 +12,14 @@ class EstatisticaMega:
         self.contagem = object
 
     def contCsv(self, local):
-        df = read_csv(local, delimiter=',')
+        df = read_csv(local, delimiter=',', header=None)
+        self.linhas = {}
 
         for index, row in df.iterrows():
-            print(row['jogador'])
-            self.linhas[f'{row['jogador']}'] = []
-            for i in range(1, 6):
-                self.linhas[f'{row['jogador']}'].append(row[f'bola{i}'])
+            row = row.sort_values()
+            self.linhas[f'{index}'] = []
+            for i in range(0, 6):
+                self.linhas[f'{index}'].append(int(row.iloc[i]) if not isna(row.iloc[i]) else None)
 
     def contXlsx(self, local):
         self.linhas = {}
@@ -79,8 +80,8 @@ def EstatisticaPorApostaJogador():
 
     pontos = 0
     dezenas = []
-    with open('./outputPontosPorConcurso.txt', 'w') as f:
-        f.write(f'Concurso;Aposta;Pontos;Apostador;Números\n')
+    with open('./outputPontosPorConcursoPorJogador.txt', 'w') as f:
+        f.write(f'Concurso;Aposta;Pontos;Jogador;Números\n')
         for i in linhaSorteada:
             for j in linhaApostada:
                 for lS in linhaSorteada[i]:
@@ -90,33 +91,34 @@ def EstatisticaPorApostaJogador():
                                 dezenas.append(int(lS))
                                 pontos += 1
 
-                apostador = ''
+                Jogador = ''
+
                 match int(j):
                     case x if x in [0,1,2,3,4,5,6,7,8,9]:
-                        apostador = ('Ricardo')
+                        Jogador = ('Ricardo')
                     case x if x in [10,11,12,13,14,15,16,17,18,19]:
-                        apostador = ('Hugo')
+                        Jogador = ('Hugo')
                     case x if x in [20,21,22,23,24,25,26,27,28,29]:
-                        apostador = ('Sergio')
+                        Jogador = ('Sergio')
                     case x if x in [30,31,32,33,34,35,36,37,38,39]:
-                        apostador = ('Cleide')
+                        Jogador = ('Cleide')
                     case x if x in [40,41,42,43,44,45,46,47,48,49]:
-                        apostador = ('Helio')
+                        Jogador = ('Helio')
                     case x if x in [50,51,52,53,54,55,56,57,58,59]:
-                        apostador = ('Luiz')
+                        Jogador = ('Luiz')
                     case x if x in [60,61,62,63,64,65,66,67,68,69]:
-                        apostador = ('Carolinda')
+                        Jogador = ('Carolinda')
                     case x if x in [70,71,72,73,74,75,76,77,78,79]:
-                        apostador = ('Leticia')
+                        Jogador = ('Leticia')
                     case x if x in [80,81,82,83,84,85,86,87,88,89]:
-                        apostador = ('Vinicius')
+                        Jogador = ('Vinicius')
                     case x if x in [90,91,92,93,94,95,96,97,98,99]:
-                        apostador = ('Michael')
+                        Jogador = ('Michael')
                     case x if x >= 100:
-                        apostador = ('Sidnei')
+                        Jogador = ('Sidnei')
 
                 if pontos >= 2: # Só registrar apostas com 2 ou mais acertos, e vc pode parametrizar isso aqui
-                    f.write(f'{(int(i) + 1)};{int(j) + 1};{pontos};{apostador};{str(dezenas)}\n')
+                    f.write(f'{(int(i))};{int(j) + 1};{pontos};{Jogador};{str(dezenas)}\n')
 
                 pontos = 0
                 dezenas = []
@@ -125,5 +127,4 @@ def EstatisticaPorApostaJogador():
 if __name__ == '__main__':
     EstatisticaPorBolaMega()
     EstatisticaPorVezesDeDezena()
-    lerArquivoCsv('./jogos.csv')
-    #EstatisticaPorApostaJogador()
+    EstatisticaPorApostaJogador()
